7be3b768b658b1b915a5624dc879bd657172677a
Qingnan Duan <qiduan@microsoft.com>
2020-10-26 17:10:03 +0800

    Implement split method for strings

---
+++ strsplit.hpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <cstdio>
+
+namespace AlgoLib::String{
+    template<typename C>
+    std::vector<std::basic_string<C>> split(std::basic_string<C> s, C splitChar){
+        int segStart = 0, index = 0;
+        std::vector<std::basic_string<C>> ret;
+
+        for(; index < s.length(); ++index){
+            if(s[index] == splitChar){
+                if(index > segStart){
+                    std::basic_string<C> segment(s, segStart, index - segStart);
+                    ret.push_back(std::move(segment));
+                }
+                segStart = index + 1;
+            }
+        }
+
+        if(index > segStart){
+            std::basic_string<C> segment(s, segStart, index - segStart);
+            ret.push_back(std::move(segment));
+        }
+
+        return ret;
+    }
+}
---
+++ strsplit_test.cpp
@@ -0,0 +1,28 @@
+#include "strsplit.hpp"
+#include <string>
+#include <cstdio>
+
+int main(){
+    std::string str = "A, B, C;";
+    std::string str2 = u8"测;试";
+    auto list = AlgoLib::String::split(str, ',');
+    if(list[0] == "A" && list[1] == " B" && list[2] == " C;"){
+
+    }
+    else{
+        printf("Test failed.\n");
+        return 1;
+    }
+
+    auto list2 = AlgoLib::String::split(str2, u8';');
+    if(list2[0] == u8"测" && list2[1] == u8"试"){
+
+    }
+    else{
+        printf("Test failed.\n");
+        return 1;
+    }
+
+    printf("strsplit test passed.\n");
+    return 0;
+}
