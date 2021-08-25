b038d64435b86f66363384c07eb4e87fb1e01e0c
Qingnan Duan <duanqn_own_1@yeah.net>
2021-08-25 15:34:52 +0800

    C++20

--- asyncExecution.hpp
+++ asyncExecution.hpp
@@ -2,9 +2,11 @@
 
 #include <functional>
 #include <thread>
+#include <concepts>
 
 namespace AlgoLib::SyntaxSugar{
-    template<typename Function, typename... Args>
+
+    template<typename Function, typename... Args> requires std::regular_invocable<Function, Args...> && std::is_same_v<std::invoke_result_t<Function, Args...>, void>
     class AsyncExecution{
         private:
         std::thread m_worker;
@@ -14,12 +16,12 @@ namespace AlgoLib::SyntaxSugar{
         virtual ~AsyncExecution();
     };
 
-    template<typename Function, typename... Args>
+    template<typename Function, typename... Args> requires std::regular_invocable<Function, Args...>&& std::is_same_v<std::invoke_result_t<Function, Args...>, void>
     AsyncExecution<Function, Args...>::AsyncExecution(bool waitUntilFinish, Function&& f, Args&&... args):
         m_worker(std::forward<Function>(f), std::forward<Args>(args)...),
         m_wait(waitUntilFinish){}
 
-    template<typename Function, typename... Args>
+    template<typename Function, typename... Args> requires std::regular_invocable<Function, Args...>&& std::is_same_v<std::invoke_result_t<Function, Args...>, void>
     AsyncExecution<Function, Args...>::~AsyncExecution(){
         if(m_wait){
             m_worker.join();
